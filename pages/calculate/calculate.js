0cac5148deed2ce73c8c40b54d1ec4750a9f4e12
Snowy <1309571292@qq.com>
2019-02-19 18:22:29 +0800

    添加data字段

--- pages/calculate/calculate.js
+++ pages/calculate/calculate.js
@@ -5,7 +5,13 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    datas:{
+      rowFir: ['C','←','%','+'],
+      rowSec:['9','8','7','-'],
+      rowThi: ['6', '5', '4','×'],
+      rowFou: ['3', '2', '1','÷'],
+      rowFiv:['0','.','=']
+    }
   },
 
   /**
@@ -14,49 +20,6 @@ Page({
   onLoad: function (options) {
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
   /**
    * 用户点击右上角分享
    */
