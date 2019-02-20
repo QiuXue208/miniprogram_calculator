2395707657b27ed39345ab6db1db330f17d8938a
Snowy <1309571292@qq.com>
2019-02-20 17:31:55 +0800

    优化处理用户输入函数

--- pages/calculate/calculate.js
+++ pages/calculate/calculate.js
@@ -13,7 +13,7 @@ Page({
       rowFou: ['3', '2', '1','÷'],
       rowFiv:['0','.','=']
     },
-    calcSign: ['%', '+', '-', '×', '÷'],
+    calcSign: ['%', '×', '÷', '+', '-'],
     number: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'],
     resultSign:'=',
     // 点击的值
@@ -26,7 +26,7 @@ Page({
     // 是否清空显示框
     clear:false,
     // 是否显示结果
-    showResult:false
+    showResult:false,
   },
 
   /**
@@ -40,44 +40,53 @@ Page({
       clickValue: e.target.id,
       showValue: this.data.showValue + e.target.id,
     })
-    this.dealInput()
+    this.dealUserInput()
   },
-  dealInput:function(){
-    // 变量
+  //用户点击的是否是calcSign数组里的符号
+  isSign:function(sign){
+    if(this.data.calcSign.indexOf(sign) === -1){
+      return false
+    }else{
+      return true
+    }
+  },
+  // 用户点击的是否是数字
+  isNumber:function(sign){
+    if (this.data.number.indexOf(sign) === -1) {
+      return false
+    }else{
+      return true
+    }
+  },
+  isPrioritySign:function(sign){
+    if(this.data.calcSign.slice(0,3).indexOf(sign) === -1){
+      return false 
+    }else{
+      return true
+    }
+  },
+  dealUserInput:function(){
     let clickValue = this.data.clickValue
     let showValue = this.data.showValue
     let length = showValue.length
-    // 倒数第二个显示的
+    // 倒数第二个显示的字符
     let lastButOne = showValue.slice(-2,-1)
+    let first = showValue.slice(0,1)
 
-    // 条件判断
     if (clickValue === 'C') {
-      // 清除键
       this.setData({
-        showValue: ''
+        showValue: '',
+        resultStr:''
       })
     } else if (clickValue === '←') {
-      // 退回一格
       this.setData({
         showValue: showValue.slice(0,length - 2)
       })
-    } else if ((lastButOne === '%' || lastButOne === '+' || lastButOne === '-' || lastButOne === '×' || lastButOne === '÷') && (clickValue === '+' || clickValue === '-' || clickValue === '×' || clickValue === '÷' || clickValue === '%')){
-      //如果连续点计算符号键，则显示第二个计算符   
+    }else if(this.isSign(lastButOne)&&this.isSign(clickValue)){
       this.setData({
-        showValue:showValue.slice(0,length -2) + clickValue
+        showValue: showValue.slice(0, length - 2) + clickValue
       })
-    } else if ((lastButOne === '' || (length === 2 &&( lastButOne === '%' || lastButOne === '+' || lastButOne === '-' || lastButOne === '×' || lastButOne === '÷'))) && clickValue === '=') {
-      // 如果一开始就点=，则报错
-      // 如果showValue长度为2，第一个为计算符号，第二个为=号，也报错
-      this.setData({
-        showValue:'错误'
-      })
-    }else if(showValue.slice(0,length-1) === '错误'){
-      // 如果显示屏上显示错误时，点击再次时按重新输入来算
-      this.setData({
-        showValue: '' + clickValue
-      })
-    } else if ((lastButOne === '%' || lastButOne === '+' || lastButOne === '-' || lastButOne === '×' || lastButOne === '÷')&& clickValue === '='){ 
+    }else if (this.isSign(lastButOne) && clickValue === '='){ 
       // 如果用户点击了类似 234+= 的情况，那么呈现的结果为234
       this.setData({
         showValue:showValue.slice(0,length -2),
@@ -92,9 +101,28 @@ Page({
         result:'',
         clear:false
       })
+    } else if ((lastButOne === '' || (length === 2 && this.isSign(lastButOne))) && clickValue === '=') {
+      // 如果一开始就点=，则报错
+      // 如果showValue长度为2，第一个为计算符号，第二个为=号，也报错
+      this.setData({
+        showValue: '错误'
+      })
+    } else if (showValue.slice(0, length - 1) === '错误') {
+      // 如果显示屏上显示错误时，点击再次时按重新输入来算
+      this.setData({
+        showValue: '' + clickValue
+      })
+    } else if(this.isPrioritySign(first) && clickValue === '='){
+      //如果第一个 是计算符 * / % ，那么最后计算的结果将报错
+      this.setData({
+        showValue:'错误'
+      })
+    }else if(clickValue === '='){
+      //如果用户点击了=号就开始计算
+
     }
   },
-  calculate:function(){
+  calculate:function(str){
 
   },
   /**
