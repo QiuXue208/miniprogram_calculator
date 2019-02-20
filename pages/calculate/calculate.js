9945984fdc29c8c3e6e6f8222caa0116a5ed4266
Snowy <1309571292@qq.com>
2019-02-20 10:15:52 +0800

    将点击的符号、数字显示到显示区

--- pages/calculate/calculate.js
+++ pages/calculate/calculate.js
@@ -5,20 +5,28 @@ Page({
    * 页面的初始数据
    */
   data: {
+    // 用于显示到界面
     datas:{
       rowFir: ['C','←','%','+'],
       rowSec:['9','8','7','-'],
       rowThi: ['6', '5', '4','×'],
       rowFou: ['3', '2', '1','÷'],
       rowFiv:['0','.','=']
-    }
+    },
+    clickValue:'',
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+  },
+  userClick:function(e){
+    console.log(e.target.id)
+    // let clickValue = this.
+    this.setData({
+      clickValue:this.data.clickValue + e.target.id
+    })
   },
   /**
    * 用户点击右上角分享
